10a9e1c82e86b1571be346eac23c4eca86749863
Victor Oliveira <victor.oliveira@gmx.com>
2018-03-19 10:07:10 -0300

    Add files via upload
    Agora é possível receber o nível de um cartucho apenas.

--- tinta_hp.py
+++ tinta_hp.py
@@ -1,4 +1,5 @@
 #!/bin/python3
+#victor.oliveira@gmx.com
 import urllib.request
 import xml.etree.ElementTree as ET
 import argparse
@@ -20,12 +21,19 @@ def get_xml(host):
 
 parser = argparse.ArgumentParser(description='Retorna o nível de tinta das impressoras HP')
 parser.add_argument('host', help='Endereço IP da impressora')
+parser.add_argument('-c', '--cartucho', help='Número do cartucho', type=int)
 args = parser.parse_args()
 
 try:
     xml = get_xml(args.host)
     colors = get_colors(xml)
-    for color in colors:
-        print('{}: {}%'.format(color, colors[color]))
+    if args.cartucho or args.cartucho == 0:
+        colors = list(colors.items())[args.cartucho]
+        print('{}: {}%'.format(colors[0], colors[1]))
+    else:
+        for color in colors:
+            print('{}: {}%'.format(color, colors[color]))
+except IndexError:
+    print('ERRO: Número do cartucho incorreto')
 except:
     print('ERRO: Não é uma impressora HP ou esta não é suportada')
