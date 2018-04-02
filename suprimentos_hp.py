368ba02c0c75c8e8373e3c5f7eac9e76fe5dfbdf
Victor Oliveira <victor.oliveira@gmx.com>
2018-04-02 17:25:44 -0300

    Add files via upload
    Adicionado suporte à impressora: HP Officejet 4500 K710.

--- suprimentos_hp.py
+++ suprimentos_hp.py
@@ -5,23 +5,36 @@ import xml.etree.ElementTree as ET
 import argparse
 
 def get_colors(host):
-    url = 'http://{}/DevMgmt/ProductUsageDyn.xml'.format(host)
-    req = urllib.request.urlopen(url)
-    raw_xml = req.read().decode()
-    xml = ET.fromstring(raw_xml)
     colors = dict()
-    for root in xml:
-        if 'ConsumableSubunit' in root.tag:
-            for ink in root:
-                for item in ink:
-                    if 'MarkerColor' in item.tag:
-                        name = item.text
-                    elif 'ConsumableRawPercentageLevelRemaining' in item.tag:
-                        percentage = item.text
-                colors.update({name : percentage})
+    try:
+        url = 'http://{}/DevMgmt/ProductUsageDyn.xml'.format(host)
+        req = urllib.request.urlopen(url)
+        raw_xml = req.read().decode()
+        xml = ET.fromstring(raw_xml)
+        for root in xml:
+            if 'ConsumableSubunit' in root.tag:
+                for ink in root:
+                    for item in ink:
+                        if 'MarkerColor' in item.tag:
+                            name = item.text
+                        elif 'ConsumableRawPercentageLevelRemaining' in item.tag:
+                            percentage = item.text
+                    colors.update({name : percentage})
+    except:
+        url = 'http://{}/index.htm?cat=info&page=printerInfo'.format(host)
+        req = urllib.request.urlopen(url)
+        page = req.read().decode()
+        if 'blackink=' in page:
+            black_index = page.find('blackink=')
+            black_level = page[black_index:black_index + 11].split('=')[1].split(';')[0]
+        if 'colorink=' in page:
+            color_index = page.find('colorink=')
+            color_level = page[color_index:color_index + 11].split('=')[1].split(';')[0]
+        colors.update({'Black' : black_level,
+                       'Tricolor' : color_level})
     return colors
 
-parser = argparse.ArgumentParser(description='Retorna o nível dos suprimentos das impressoras em rede HP')
+parser = argparse.ArgumentParser(description='Retorna o nível de tinta das impressoras em rede da HP')
 parser.add_argument('host', help='Endereço IP da impressora')
 parser.add_argument('-t', '--tinta', help='Número da tinta', type=int)
 args = parser.parse_args()
